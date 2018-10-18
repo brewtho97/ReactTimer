adcb6e459ee5cb36e7cbc8fcf211d2eaba9cccc6
Thomas Brewer <brewtho@nlg.nhs.uk>
2018-10-18 13:58:03 +0100

    Added new components to navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const Timer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Timer Component</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
