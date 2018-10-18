8e22caed7adb5a640b0e75762e74f3bbfc24cc57
Thomas Brewer <brewtho@nlg.nhs.uk>
2018-10-18 17:04:35 +0100

    Added CountdownForm component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,15 +3,15 @@ const Nav = require('Nav');
 
 const Main = (props) => {
     return (
-       <div>
-           <div>
-               <div>
-                   <Nav/>
-                   <p>Main.jsx Rendered</p>
-                   {props.children}
-               </div>
+     <div>
+        <Nav/>
+         <div className="row">
+           <div className="column small-centered medium-6 large-4">
+             <p>Main.jsx Rendered</p>
+             {props.children}
            </div>
-       </div> 
+         </div>
+     </div> 
     );
 }
 
